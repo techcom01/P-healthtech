07d23d7c6be5a85a69f7485b1100aa3056e7f1f4
Chanchal <chanchal.sharma27@gmail.com>
2023-03-20 21:20:26 +0530

    style file updated

--- wp-content/themes/oms/404.php
+++ wp-content/themes/oms/404.php
@@ -10,51 +10,12 @@
 get_header();
 ?>
 
-	<main id="primary" class="site-main">
-
-		<section class="error-404 not-found">
-			<header class="page-header">
-				<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'oms' ); ?></h1>
-			</header><!-- .page-header -->
-
-			<div class="page-content">
-				<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'oms' ); ?></p>
-
-					<?php
-					get_search_form();
-
-					the_widget( 'WP_Widget_Recent_Posts' );
-					?>
-
-					<div class="widget widget_categories">
-						<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'oms' ); ?></h2>
-						<ul>
-							<?php
-							wp_list_categories(
-								array(
-									'orderby'    => 'count',
-									'order'      => 'DESC',
-									'show_count' => 1,
-									'title_li'   => '',
-									'number'     => 10,
-								)
-							);
-							?>
-						</ul>
-					</div><!-- .widget -->
-
-					<?php
-					/* translators: %1$s: smiley */
-					$oms_archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'oms' ), convert_smilies( ':)' ) ) . '</p>';
-					the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$oms_archive_content" );
-
-					the_widget( 'WP_Widget_Tag_Cloud' );
-					?>
-
-			</div><!-- .page-content -->
-		</section><!-- .error-404 -->
-
-	</main><!-- #main -->
+<section class="content-padding notfound">
+	<div class="container">
+		<h1>404 error</h1>
+		<h2>Page not working.<br>We couldn’t find this page</h2>
+	</div>
+</section>
 
 <?php
 get_footer();
